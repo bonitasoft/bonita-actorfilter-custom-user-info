f7eb8feaea52465744733f9a5d445c6cd74bafdb
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-11-27 15:28:38 +0100

    chore(build): use java 17 (#4)
    Closes BPM-18

--- src/test/java/org/bonitasoft/actorfilter/custom/user/info/CustomUserInfoUserFilterTest.java
+++ src/test/java/org/bonitasoft/actorfilter/custom/user/info/CustomUserInfoUserFilterTest.java
@@ -1,5 +1,17 @@
 package org.bonitasoft.actorfilter.custom.user.info;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.bonitasoft.engine.api.APIAccessor;
 import org.bonitasoft.engine.api.IdentityAPI;
 import org.bonitasoft.engine.api.ProcessAPI;
@@ -8,22 +20,11 @@ import org.bonitasoft.engine.connector.EngineExecutionContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 class CustomUserInfoUserFilterTest {
 
     private static final String INFO_NAME_KEY = "customUserInfoName";
